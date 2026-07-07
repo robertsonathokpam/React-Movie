e8fd41247e58b724e89b92c2f548a4968e1fb0b3
robertsonathokpam <153417457+robertsonathokpam@users.noreply.github.com>
2026-07-07 11:34:54 +0530

    Fix API error handling

--- movie/src/services/api.js
+++ movie/src/services/api.js
@@ -1,19 +1,19 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL="https://api.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";
 
-export const getPopularMovies = async() =>{
+export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     const data = await response.json();
     return data.results;
 };
 
-export const searchMovies = async(query) =>{
+export const searchMovies = async (query) => {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
     const data = await response.json();
     return data.results;
 };
 
-export const getMovieDetails = async(id) => {
+export const getMovieDetails = async (id) => {
     const response = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
     const data = await response.json();
     return data;
